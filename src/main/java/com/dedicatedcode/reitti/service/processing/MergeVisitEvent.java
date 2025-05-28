42607c29f1a179aafdb30c60ccec10e0279a2a2d
Daniel Graf <daniel@dedicatedcode.com>
2025-05-28 16:56:53 +0200

    - Remove unused imports in `MergeVisitEvent` class - Optimize code readability

--- src/main/java/com/dedicatedcode/reitti/service/processing/MergeVisitEvent.java
+++ src/main/java/com/dedicatedcode/reitti/service/processing/MergeVisitEvent.java
@@ -1,10 +1,7 @@
 package com.dedicatedcode.reitti.service.processing;
 
-import org.springframework.messaging.core.MessagePostProcessor;
-
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Map;
 
 public record MergeVisitEvent(Long userId, Instant startTime, Instant endTime) implements Serializable {
 }
