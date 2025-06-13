d4ed3b6dc3c12799faa6025adb7bf4f97128e8f9
Daniel Graf <daniel@dedicatedcode.com>
2025-06-13 05:47:37 +0200

    added missing import

--- src/main/java/com/dedicatedcode/reitti/controller/SettingsController.java
+++ src/main/java/com/dedicatedcode/reitti/controller/SettingsController.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 
