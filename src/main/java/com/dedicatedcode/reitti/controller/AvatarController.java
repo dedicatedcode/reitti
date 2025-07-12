bef8489c225a435ae74c020ac5ee2cdeed1d02e9
Daniel Graf <daniel@dedicatedcode.com>
2025-07-12 15:02:44 +0200

    enhancement(#114): add default avatar images and add alt image fallback to avatar display

--- src/main/java/com/dedicatedcode/reitti/controller/AvatarController.java
+++ src/main/java/com/dedicatedcode/reitti/controller/AvatarController.java
@@ -1,22 +1,15 @@
 package com.dedicatedcode.reitti.controller;
 
-import com.dedicatedcode.reitti.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.IOException;
 import java.util.Map;
-import java.util.Objects;
 
 @Controller
 @RequestMapping("/avatars")
