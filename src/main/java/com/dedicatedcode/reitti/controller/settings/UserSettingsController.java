0fea11f26b6c33f60d073ae85def8e6d33b604fb
Daniel Graf <daniel@dedicatedcode.com>
2025-09-27 07:44:18 +0200

    enhancement(#283): fixed disabling new user button function

--- src/main/java/com/dedicatedcode/reitti/controller/settings/UserSettingsController.java
+++ src/main/java/com/dedicatedcode/reitti/controller/settings/UserSettingsController.java
@@ -163,6 +163,7 @@ public class UserSettingsController {
         model.addAttribute("users", users);
         model.addAttribute("currentUsername", authentication.getName());
         model.addAttribute("isAdmin", true);
+        model.addAttribute("addUserAvailable", !localLoginDisabled);
 
         // Return the users-list fragment
         return "fragments/user-management :: users-list";
@@ -226,6 +227,7 @@ public class UserSettingsController {
         model.addAttribute("users", users);
         model.addAttribute("currentUsername", authentication.getName());
         model.addAttribute("isAdmin", true);
+        model.addAttribute("addUserAvailable", !localLoginDisabled);
 
         // Return the users-list fragment
         return "fragments/user-management :: users-list";
@@ -323,6 +325,7 @@ public class UserSettingsController {
             model.addAttribute("users", users);
             model.addAttribute("currentUsername", isCurrentUser ? username : currentUsername);
             model.addAttribute("isAdmin", true);
+            model.addAttribute("addUserAvailable", !localLoginDisabled);
             return "fragments/user-management :: users-list";
         } else {
             // For regular users, return their updated form
