7256f8c84f53e1bf403ea7a2351ef27ad02fa478
Daniel Graf <daniel@dedicatedcode.com>
2025-11-02 05:44:18 +0100

    bugfix(#384): removed ReloadableMessageSource because this always falls back to the system locale if locale is not found. And en is not found because there is no messages_en.properties. Added russian back

--- src/main/java/com/dedicatedcode/reitti/config/WebConfig.java
+++ src/main/java/com/dedicatedcode/reitti/config/WebConfig.java
@@ -2,14 +2,11 @@ package com.dedicatedcode.reitti.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
@@ -32,25 +29,15 @@ public class WebConfig implements WebMvcConfigurer {
         resolver.setCookiePath("/");
         return resolver;
     }
-    
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
-        interceptor.setParamName("lang");
-        return interceptor;
-    }
-    
-    @Bean
-    public ReloadableResourceBundleMessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setCacheSeconds(3600);
-        return messageSource;
-    }
-    
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
-    }
+//
+//
+//    @Bean
+//    public ReloadableResourceBundleMessageSource messageSource() {
+//        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+//        messageSource.setBasename("classpath:messages");
+//        messageSource.setDefaultEncoding("UTF-8");
+//        messageSource.setCacheSeconds(3600);
+//        return messageSource;
+//    }
+
 }
