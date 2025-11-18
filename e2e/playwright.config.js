5e64cb2d5599452166504ea14ab89846b6532f6b
Daniel Graf <daniel@dedicatedcode.com>
2025-11-18 16:27:56 +0100

    bugfix(#472): removed old mvn build

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import {defineConfig, devices} from '@playwright/test';
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -10,11 +10,11 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["list"], ["html"], ["@midleman/github-actions-reporter"]],
+  reporter: process.env.CI ? [["@midleman/github-actions-reporter"]] : [["list"], ["html"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
