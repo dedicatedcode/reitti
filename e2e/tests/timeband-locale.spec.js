9651ad97469d290079b19bcda87ac80513169a23
Daniel Graf <daniel@dedicatedcode.com>
2025-12-01 03:32:49 +0100

    bugfix(#506): added more e2e tests for timezone related date picker selection

--- e2e/tests/timeband-locale.spec.js
+++ e2e/tests/timeband-locale.spec.js
@@ -35,8 +35,8 @@ test.describe('Date Picker Tests', () => {
         await page.waitForNavigation();
     });
 
-    test('should load correct date when in a different timezone', async ({page}) => {
-        await page.clock.setFixedTime(new Date('2017-02-02T01:00:00'));
+    test('should load correct today when in a different timezone', async ({page}) => {
+        await page.clock.setFixedTime(new Date('2017-02-02T00:30:00'));
 
         const responsePromise = page.waitForResponse('**/timeline/content/range?startDate=2017-02-02&endDate=2017-02-02&timezone=Europe%2FBerlin');
 
@@ -44,6 +44,32 @@ test.describe('Date Picker Tests', () => {
         await expect(page.locator('div').filter({ hasText: 'Thu2Feb' }).nth(2)).toContainClass('selected');
         const response = await responsePromise;
         expect(response.status()).toBe(200);
+    })
+
+    test('should load correct date when in a different timezone at start of day', async ({page}) => {
+        await page.clock.setFixedTime(new Date('2017-02-02T00:30:00'));
+
+        const responsePromise = page.waitForResponse('**/timeline/content/range?startDate=2017-12-20&endDate=2017-12-20&timezone=Europe%2FBerlin');
+
+        await page.getByText('Wed20Dec').click()
+        await expect(page.locator('div').filter({ hasText: 'Wed20Dec' }).nth(2)).toContainClass('selected');
+        const response = await responsePromise;
+        expect(response.status()).toBe(200);
+
+        await expect(page).toHaveURL(/startDate=2017-12-20&endDate=2017-12-20/);
+    })
+
+    test('should load correct date when in a different timezone at end of day', async ({page}) => {
+        await page.clock.setFixedTime(new Date('2017-02-02T23:30:50'));
+
+        const responsePromise = page.waitForResponse('**/timeline/content/range?startDate=2017-12-20&endDate=2017-12-20&timezone=Europe%2FBerlin');
+
+        await page.getByText('Wed20Dec').click()
+        await expect(page.locator('div').filter({ hasText: 'Wed20Dec' }).nth(2)).toContainClass('selected');
+        const response = await responsePromise;
+        expect(response.status()).toBe(200);
+
+        await expect(page).toHaveURL(/startDate=2017-12-20&endDate=2017-12-20/);
     })
 
 
