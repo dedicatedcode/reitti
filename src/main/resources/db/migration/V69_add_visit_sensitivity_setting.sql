e31b1d2fefc2a4e6cd5aec4c1347b7a7913c7ea3
Daniel Graf <daniel@dedicatedcode.com>
2025-12-07 15:36:21 +0100

    feat(#536): add migration for new visit sensitivity settings
    - Add SQL migration script to configure visit sensitivity parameters for all users

---
+++ src/main/resources/db/migration/V69_add_visit_sensitivity_setting.sql
@@ -0,0 +1,15 @@
+INSERT INTO visit_detection_parameters(user_id, valid_since, detection_minimum_stay_time_seconds,
+                                       detection_max_merge_time_between_same_stay_points,
+                                       merging_search_duration_in_hours, merging_max_merge_time_between_same_visits,
+                                       merging_min_distance_between_visits, density_max_interpolation_distance_meters,
+                                       density_max_interpolation_gap_minutes)
+SELECT id,
+       now(),
+       300,
+       300,
+       24,
+       300,
+       100,
+       50,
+       720
+FROM users;
