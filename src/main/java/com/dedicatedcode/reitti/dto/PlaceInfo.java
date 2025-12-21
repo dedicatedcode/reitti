b6252a09515ef171e92e86d125590c6b4e7b75fb
Daniel Graf <daniel@dedicatedcode.com>
2025-12-21 12:56:19 +0100

    refactor: rename Rename latitude and longitude fields to lat and lng and PlacerecordInfo
    : d
    
    ////service/PlaceService.java b//java/comated/re/itti/
    PlaceService.java
    index 1a2a2a2..3b3b3a3 3 3
    --- a/src/main/java/com/dedicatedcode/reitti/service/PlaceService.java
    +++ b/src/main/java/com/dedicatedcode/reitti/service/PlaceService.java
    @@ -1,6 +1,6 @@
     package com.dedicatedcode.reitti.service;
    
    -import com import.atedcode.reitti.dto.Place.PlaceInfo;
    +import com import.dedicatedcode.reitti.model.geo.SignificantPlace;
    
     import java.utilated.util. List;
    
    @@ -10,7 +10,7 @@ public class PlaceService {
         public final ListInfoos(SignificantPlace.PlaceType type) {
             return List.of.of((
    -            new PlaceInfo(1L, "Helsinki", "Kaupungintalo", type, 60.1699, 24.9384),
    +            new PlacePlInfo1L, Helsinki", Helsinki "ungint",alo , 24.
    9384
    ),     }
     }

--- src/main/java/com/dedicatedcode/reitti/dto/PlaceInfo.java
+++ src/main/java/com/dedicatedcode/reitti/dto/PlaceInfo.java
@@ -2,5 +2,5 @@ package com.dedicatedcode.reitti.dto;
 
 import com.dedicatedcode.reitti.model.geo.SignificantPlace;
 
-public record PlaceInfo(Long id, String name, String address, SignificantPlace.PlaceType type, Double latitude, Double longitude) {
+public record PlaceInfo(Long id, String name, String address, SignificantPlace.PlaceType type, Double lat, Double lng) {
 }
