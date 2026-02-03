074f7de88088ac1c58302017628b0e8469ae24f4
Daniel Graf <daniel@dedicatedcode.com>
2026-02-03 09:52:27 +0100

    698 feature request avoid using keys for redis (#737)

--- src/main/java/com/dedicatedcode/reitti/config/AppConfig.java
+++ src/main/java/com/dedicatedcode/reitti/config/AppConfig.java
@@ -5,6 +5,10 @@ import org.locationtech.jts.geom.PrecisionModel;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.BatchStrategies;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,5 +23,17 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer(RedisConnectionFactory connectionFactory) {
+        return (builder) -> {
+            builder.cacheWriter(
+                    RedisCacheWriter.nonLockingRedisCacheWriter(
+                            connectionFactory,
+                            BatchStrategies.scan(1000)
+                    )
+            );
+        };
     }
 }
