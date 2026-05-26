9eab0bf91c212382e56654a01a248c055744e73e
Daniel Graf <daniel@dedicatedcode.com>
2026-05-26 07:13:13 +0200

    refactor(map-styles): reformat

--- src/main/java/com/dedicatedcode/reitti/controller/MapStyleControllerAdvice.java
+++ src/main/java/com/dedicatedcode/reitti/controller/MapStyleControllerAdvice.java
@@ -4,9 +4,6 @@ import com.dedicatedcode.reitti.dto.MapLibreStyleDefinition;
 import com.dedicatedcode.reitti.model.security.User;
 import com.dedicatedcode.reitti.repository.UserMapStyleJdbcService;
 import com.dedicatedcode.reitti.service.MapLibreMapStylesService;
-import com.dedicatedcode.reitti.service.RequestHelper;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,14 +15,12 @@ public class MapStyleControllerAdvice {
 
     private final MapLibreMapStylesService mapLibreMapStylesService;
     private final UserMapStyleJdbcService userMapStyleJdbcService;
-    private final ObjectMapper objectMapper;
 
-    public MapStyleControllerAdvice(MapLibreMapStylesService mapLibreMapStylesService, UserMapStyleJdbcService userMapStyleJdbcService, ObjectMapper objectMapper) {
+    public MapStyleControllerAdvice(MapLibreMapStylesService mapLibreMapStylesService,
+                                    UserMapStyleJdbcService userMapStyleJdbcService) {
         this.mapLibreMapStylesService = mapLibreMapStylesService;
         this.userMapStyleJdbcService = userMapStyleJdbcService;
-        this.objectMapper = objectMapper;
     }
-
 
     @ModelAttribute("mapStylesJson")
     public List<MapLibreStyleDefinition> getMapStylesConfiguration(@AuthenticationPrincipal User user) {
