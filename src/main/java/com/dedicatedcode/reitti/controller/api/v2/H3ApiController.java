c88832956447c162048baed79c921264eb64332c
Daniel Graf <daniel@dedicatedcode.com>
2026-07-19 11:12:56 +0200

    feat(1114): add H3 API controller
    - Introduce `H3ApiController` with REST support at `/api/v2/h3`.
    - Add minor formatting adjustments in `LocationApiController`.

---
+++ src/main/java/com/dedicatedcode/reitti/controller/api/v2/H3ApiController.java
@@ -0,0 +1,7 @@
+package com.dedicatedcode.reitti.controller.api.v2;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v2/h3")
+public class H3ApiController {
+}
