ecbfe7e424bc5d09fd5f682ad77336b31abd5685
Daniel Graf <daniel@dedicatedcode.com>
2026-08-28 08:24:52 +0200

    bugfix(#1212) Remove unused Timer import from LocationPointStagingService

--- src/main/java/com/dedicatedcode/reitti/service/processing/LocationPointStagingService.java
+++ src/main/java/com/dedicatedcode/reitti/service/processing/LocationPointStagingService.java
@@ -18,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.sql.*;
 import java.util.List;
 import java.util.Set;
-import java.util.Timer;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
